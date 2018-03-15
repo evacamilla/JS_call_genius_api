a25fc083ab0a05af723f2f7f8d547f96f685bcce
cami <evacamilla.s@gmail.com>
2018-03-15 10:36:01 +0100

    change some syntax. and make a functionn for making the request to the api..

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
-//access token for security
-const myToken = '-vz2WiwO-REtrw3MgtBkeIY5ZG99GvAle3OMLWUgY3z09YcvU3saof76eum-FvcE';
-
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 const searchResultsUl = document.getElementById('searchResultsUl');
 
+    
+function createRequest(apiURL){
+    //access token for security
+    const myToken = '-vz2WiwO-REtrw3MgtBkeIY5ZG99GvAle3OMLWUgY3z09YcvU3saof76eum-FvcE';
 
+    var request = new Request(apiURL, {
+        headers: new Headers({
+            'Authorization': `Bearer ${myToken}`
+        })
+    });
+    return request;
+}
 
 searchButton.addEventListener('click', function() {
     const searchQuery = searchInput.value;
-    const apiURL = 'https://cors-anywhere.herokuapp.com/https://api.genius.com/search?q=' + searchQuery;
-    
-    var request = new Request(apiURL, {
-        headers: new Headers({
-            'Authorization': 'Bearer ' + myToken
-        })
-    });
+    const apiURL = `https://cors-anywhere.herokuapp.com/https://api.genius.com/search?q=${searchQuery}`;
+
+    request = createRequest(apiURL);
 
     fetch(request)
         .then(function(response) {
