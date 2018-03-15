aed2c6bcc740240b912995509b28dd54fdef5935
cami <evacamilla.s@gmail.com>
2018-03-15 10:10:25 +0100

    calling the api with authorization works, using my token key from genius.comand cors herokuapp.. not very safe

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+//access token for security
+const myToken = '-vz2WiwO-REtrw3MgtBkeIY5ZG99GvAle3OMLWUgY3z09YcvU3saof76eum-FvcE';
+const apiURL = 'https://cors-anywhere.herokuapp.com/https://api.genius.com/search?q=guleed';
+
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 const searchResultsUl = document.getElementById('searchResultsUl');
+
+var request = new Request(apiURL, {
+    headers: new Headers({
+        'Authorization': 'Bearer ' + myToken
+    })
+});
+
+
+fetch(request)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        //console.log('id: ' + data.response.song.producer_artists[0].id);
+        //console.log('name: ' + data.response.song.producer_artists[0].name);
+    });
+
+
