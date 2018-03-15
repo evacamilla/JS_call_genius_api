ec9d76018bae5b93908fe5a79c384a22dac7f86a
cami <evacamilla.s@gmail.com>
2018-03-15 10:27:19 +0100

    store value from searchinput and put it in the request to the api..

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 //access token for security
 const myToken = '-vz2WiwO-REtrw3MgtBkeIY5ZG99GvAle3OMLWUgY3z09YcvU3saof76eum-FvcE';
-const apiURL = 'https://cors-anywhere.herokuapp.com/https://api.genius.com/search?q=guleed';
 
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 const searchResultsUl = document.getElementById('searchResultsUl');
 
-var request = new Request(apiURL, {
-    headers: new Headers({
-        'Authorization': 'Bearer ' + myToken
-    })
-});
 
 
 searchButton.addEventListener('click', function() {
+    const searchQuery = searchInput.value;
+    const apiURL = 'https://cors-anywhere.herokuapp.com/https://api.genius.com/search?q=' + searchQuery;
+    
+    var request = new Request(apiURL, {
+        headers: new Headers({
+            'Authorization': 'Bearer ' + myToken
+        })
+    });
+
     fetch(request)
         .then(function(response) {
             return response.json();
