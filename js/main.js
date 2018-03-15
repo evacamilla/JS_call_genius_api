4906e9ca80f786140cd0e7d2d805ab0e8a155239
cami <evacamilla.s@gmail.com>
2018-03-15 10:01:29 +0100

    get the html elements needed for search and display by id

---
+++ js/main.js
@@ -0,0 +1,3 @@
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const searchResultsUl = document.getElementById('searchResultsUl');
